4451852144ee63af28c54aeb67e39886d9bc566d
arpit-mittal-15 <arpitmittaldhn@gmail.com>
2025-07-09 22:35:36 +0530

    recoil

--- week7/contextapi/src/context.jsx
+++ week7/contextapi/src/context.jsx
@@ -3,3 +3,6 @@ import { createContext } from "react";
 const newContext = createContext(0);
 
 export default newContext;
+
+// the problem is it still re renders all the components.
+// so we use state management libs.
---
+++ week7/recoil/src/App.jsx
@@ -0,0 +1,44 @@
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
+
+function App() {
+  return (
+    <div>
+        <Count />
+    </div>
+  )
+}
+
+function Count(){
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  )
+}
+
+function CountRenderer(){
+  const count = useRecoilValue(countAtom);
+  
+  return(
+    <div>
+      <b>
+        {count}
+      </b>
+    </div>
+  )
+}
+
+function Buttons(){
+  const [count, setCount] = useRecoilState(countAtom);
+
+  return (
+    <div>
+      <button onClick={() => {setCount(count + 1)}}>Increase</button>
+      <button onClick={() => {setCount(count - 1)}}>Decrease</button>
+    </div>
+  )
+}
+
+export default App
