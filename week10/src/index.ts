bb4da29da0ec05da80beb072170540da9cc9bb77
arpit-mittal-15 <arpitmittaldhn@gmail.com>
2025-09-29 18:02:54 +0530

    prisma

--- week10/src/index.ts
+++ week10/src/index.ts
@@ -17,3 +17,45 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
+async function insertUser(username: string, password: string, firstName: string, lastName: string, email: string){
+  const response = await prisma.user.create({
+    data: {
+      email,
+      username,
+      firstName,
+      lastName,
+      password
+    }
+  });
+  console.log(response);
+}
+// insertUser("test","test","test","test","test");
+
+async function insertTodo(title: string, description: string, userId: number){
+  const response = await prisma.todo.create({
+    data: {
+      title,
+      description,
+      userId
+    }
+  });
+  console.log(response);
+};
+// insertTodo("Prisma", "Prisma desc", 1);
+
+async function getTodosAndUserDetails(userId: number){
+  const response = await prisma.todo.findMany({
+    where: {
+      userId
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      user: true
+    }
+  })
+  console.log(response);
+}
+
+getTodosAndUserDetails(1);
